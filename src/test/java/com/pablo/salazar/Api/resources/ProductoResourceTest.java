1ba40aa926dc8d862b5f488bfcb19f17190c6a38
Pablo Salazar <pablo.s.1840@gmail.com>
2021-06-24 10:04:45 -0500

    do the tests of the first two methods for search all products and search the product by Id

--- src/test/java/com/pablo/salazar/Api/resources/ProductoResourceTest.java
+++ src/test/java/com/pablo/salazar/Api/resources/ProductoResourceTest.java
@@ -1,7 +1,37 @@
 package com.pablo.salazar.Api.resources;
 
-import static org.junit.Assert.*;
+import com.pablo.salazar.Api.entities.Producto;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 
 public class ProductoResourceTest {
 
+    @Autowired
+    private RestService restService;
+    private Producto producto;
+
+    @Test
+    public void getAllProduct() {
+        String json = restService
+                .restBuilder(new RestBuilder<String>().clazz(String.class))
+                .path(ProductoResource.PRODUCTO).get().build();
+        System.out.println(json);
+    }
+
+    @Test
+    public void getById() {
+        String json = restService
+                .restBuilder(new RestBuilder<String>().clazz(String.class))
+                .path(ProductoResource.PRODUCTO).path(ProductoResource.ID).expand(10).get().build();
+        System.out.println(json);
+    }
 }
